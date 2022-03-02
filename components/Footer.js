40b304dc0fc0f4aa987a013be4025dd4b12562b7
zeedtanue <zeedtanue@gmail.com>
2022-03-02 13:43:26 +0600

    header and footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,16 +1,75 @@
 import React from 'react'
-
+import Styles from '../styles/Footer.module.css'
 function Footer() {
     return (
-        <div>
-            <footer className="footer">
-                <div className="content has-text-centered">
-                <p>
-                    <strong>Let's Go My</strong> by <a href="https://www.linkedin.com/in/tamzeed-hossain-475852171/">Tamzeed Hossain</a> 
-                     <br/>copyright 2021 © Tamzeed Hossain. All Right Reserved
-                </p>
+        <div className={Styles.container}>
+            <footer className={`footer`, Styles.footer} >
+            <div className="container">
+                <div className="columns">
+                    <div className="column is-3 is-offset">
+                        {/* <p><strong>Revorium</strong></p> */}
+                        <p style={{ fontSize: 22, fontWeight:'bold' }}> Revorium</p>
+                        <p>Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. </p>
+                    </div>
+                    <div className="column is-2 is-offset-1">
+                    <p style={{ fontSize: 22, fontWeight:'bold' }} className={Styles.text} >Our Service</p>
+
+                        <p>Pricing</p>
+                        <p>Tracking</p>
+                        <p>Report A Bug</p>
+                        <p>Terms of Service</p>
+
+
+                    </div>
+                    <div className="column is-2 is-offset-1">
+                    <p style={{ fontSize: 22, fontWeight:'bold' }} className={Styles.text} >Our Company</p>
+                        <p>Reporting</p>
+                        <p>Get in Touch</p>
+                        <p>Management</p>
+                        <p>Terms of Service</p>
+
+
+                    </div>
+                    <div className="column is-2 is-offset-1">
+                    <p style={{ fontSize: 22, fontWeight:'bold' }}>Address</p>
+                        <p>2972 Westheimer Rd. Santa Ana, Illinois 85486 </p> 
+                        <p>(270) 555-0117</p>
+                        <p>tanya.hill@example.com</p>
+
+
+                    </div>
                 </div>
-            </footer>
+                <div className='mgt-medium'>
+
+                </div>
+                <div className='mgt-medium'>
+
+                </div>
+
+                <div style={{ marginTop: 70 }}>
+                  <div className="column is-5 is-offset">
+                      <p>© 2022 Revorium. A European IT company. All rights  served.</p>
+                  </div>
+                  <div className='column is-1'>
+                      <span className="icon has-text-info">
+                        <i className="fas fa-info-circle"></i>
+                      </span>
+                      <span className="icon has-text-success">
+                        <i className="fas fa-check-square"></i>
+                      </span>
+                      <span className="icon has-text-warning">
+                        <i className="fas fa-exclamation-triangle"></i>
+                      </span>
+                      <span className="icon has-text-danger">
+                        <i className="fas fa-ban"></i>
+                      </span>
+                  </div>
+                  
+                </div>
+                
+            </div>
+            <script src="../js/bulma.js"></script>
+        </footer>
         </div>
     )
 }
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,20 +1,19 @@
 import React,{useEffect,useState} from 'react'
 import Link from 'next/link'
 
-
 const NavBar = () => {
     
     const [isActive, setisActive] = useState(false);
 
     return (
         <div>  
-            <nav className="navbar is-tranparent is-bold">
+            <nav style={{ height: 30, padding:20 }} className="container navbar is-tranparent is-bold">
                 <div className="container">
                     <Link passHref href={"/"}>
                 <div className="navbar-brand">
                     
                     <a className="navbar-item is-size-2 has-text-weight-bold" href="../">
-                    <img src="/logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="268" height="100"/>
+                    <img style={{ height:300  , width:200 }} src="/logo.png" alt="Bulma: a modern CSS framework based on Flexbox" width="268" height="100"/>
 
                     </a>
                     <div class="navbar-burger burger" data-target="navbarExampleTransparentExample">
@@ -32,29 +31,29 @@ const NavBar = () => {
                 <div id="navbarExampleTransparentExample" className={`navbar-menu ${isActive ? "is-active" : ""}`}>
                   <div className="navbar-end">
                     <Link passHref href={'/'}>
-                        <a className='navbar-item'>
+                        <a style={{ fontSize:14, font: 'Nunito' }} className='navbar-item'>
                             HOME
                         </a>
                     </Link>
                     <div className="navbar-item has-dropdown is-hoverable">
                       <Link passHref href={'/service'}>
                     
-                        <a className="navbar-link">
+                        <a  style={{ fontSize:14, font: 'Nunito' }} className="navbar-link">
                           SERVICE
                         </a>
                       </Link>
                       <div className="navbar-dropdown">
                       <Link passHref href={'/service/Service 1'}>
-                          <a className="navbar-item">
+                          <a style={{ fontSize:14, font: 'Nunito' }} className="navbar-item">
                             Service 1
                           </a>
                         </Link>
                         <Link passHref href={'/service/Service 2'}>
-                          <a className="navbar-item">
+                          <a style={{ fontSize:14, font: 'Nunito' }} className="navbar-item">
                             Service 2
                           </a>
                         </Link><Link passHref href={'/service/service 2'}>
-                          <a className="navbar-item">
+                          <a style={{ fontSize:14, font: 'Nunito' }} className="navbar-item">
                             Service 3
                           </a>
                         </Link>
@@ -63,23 +62,23 @@ const NavBar = () => {
                     </div>
                     <div className="navbar-item has-dropdown is-hoverable">
                       <Link passHref href={'/our-products'}>
-                        <a className="navbar-link" href="https://bulma.io/documentation/overview/start/">
+                        <a style={{ fontSize:14, font: 'Nunito' }} className="navbar-link" href="https://bulma.io/documentation/overview/start/">
                           OUR PRODUCTS
                         </a>
                       </Link>
                       
                       <div className="navbar-dropdown">
                         <Link passHref href={'/our-products/Product 1'}>
-                          <a className="navbar-item">
+                          <a style={{ fontSize:14, font: 'Nunito' }} className="navbar-item">
                             Product 1
                           </a>
                         </Link>
                         <Link passHref href={'/our-products/Product 2'}>
-                          <a className="navbar-item">
+                          <a style={{ fontSize:14, font: 'Nunito' }} className="navbar-item">
                             Product 2
                           </a>
                         </Link><Link passHref href={'/our-products/Product 3'}>
-                          <a className="navbar-item">
+                          <a style={{ fontSize:14, font: 'Nunito' }} className="navbar-item">
                             Product 1
                           </a>
                         </Link>
@@ -87,17 +86,17 @@ const NavBar = () => {
                         </div>
                     </div>
                     <Link passHref href={'/blog'}>
-                        <a className='navbar-item'>
+                        <a style={{ fontSize:14, font: 'Nunito' }} className='navbar-item'>
                             BLOG
                         </a>
                     </Link>
                     
                     <Link passHref href={'/company'}>
-                        <a className='navbar-item'>
+                        <a style={{ fontSize:14, font: 'Nunito' }}  className='navbar-item'>
                             COMPANY
                         </a>
                     </Link><Link passHref href={'/contact-us'}>
-                        <a className='navbar-item'>
+                        <a style={{ fontSize:14, font: 'Nunito' }} className='navbar-item'>
                             CONTACT US
                         </a>
                     </Link>
