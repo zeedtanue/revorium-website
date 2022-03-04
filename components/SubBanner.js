6bf04cd9b90f8b4aa58429caa93e19d3a704bf09
zeedtanue <zeedtanue@gmail.com>
2022-03-04 18:27:11 +0600

    className

--- components/SubBanner.js
+++ components/SubBanner.js
@@ -2,9 +2,9 @@ import React from 'react'
 
 export const SubBanner = () => {
   return (
-    <section class="hero is-medium is-link has-text-centered">
-        <div class="hero-body subpagebanner-body Home_subpagebanner-body__zGm2F">
-            <p class="title">
+    <section className="hero is-medium is-link has-text-centered">
+        <div className="hero-body subpagebanner-body Home_subpagebanner-body__zGm2F">
+            <p className="title">
              Contact Us
             </p>
             <p className='subtitle'>
